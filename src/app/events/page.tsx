f2fddb611529b938be8c54a137cc26d885f66a0d
Kirill Bondarenko <billionmann@gmail.com>
2024-03-31 12:10:08 +0900

    feat: mocks added

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,31 +1,7 @@
 import { EventCard } from "@/components/cards/event-card";
+import { events } from "@/mocks/events";
 
-import event2 from "@/assets/img/backgrounds/event2.png";
-import event3 from "@/assets/img/backgrounds/event3.png";
 import Link from "next/link";
-
-export const events = [
-  {
-    id: "1",
-    name: "Lil uzi vert’s concert",
-    location: {
-      city: "Lisbon",
-      placement: "Rolling Loud",
-    },
-    time: "21:00 AM",
-    coverSrc: event2.src,
-  },
-  {
-    id: "2",
-    name: "Lana Del Rey’s concert",
-    location: {
-      city: "Leeds",
-      placement: "Leeds Festival",
-    },
-    time: "19:00 AM",
-    coverSrc: event3.src,
-  },
-];
 
 export default function Events() {
   return (
---
+++ src/mocks/events.ts
@@ -0,0 +1,25 @@
+import event2 from "@/assets/img/backgrounds/event2.png";
+import event3 from "@/assets/img/backgrounds/event3.png";
+
+export const events = [
+  {
+    id: "1",
+    name: "Lil uzi vert’s concert",
+    location: {
+      city: "Lisbon",
+      placement: "Rolling Loud",
+    },
+    time: "21:00 AM",
+    coverSrc: event2.src,
+  },
+  {
+    id: "2",
+    name: "Lana Del Rey’s concert",
+    location: {
+      city: "Leeds",
+      placement: "Leeds Festival",
+    },
+    time: "19:00 AM",
+    coverSrc: event3.src,
+  },
+];
